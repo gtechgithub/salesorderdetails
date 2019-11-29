30fef8d685d07c7409aa526a85009f9df62e594d
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-11-29 23:21:13 +0800

    Create Constants.java

---
+++ com/storeorder/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.storeorder.constants;
+
+public interface Constants {
+	public static final String ddmmyyydateFormatInCSV = "dd.MM.yy";
+	public static final String ddmmyyydateFormatInDB = "yyyy-MM-dd";
+}
