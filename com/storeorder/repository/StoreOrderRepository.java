26fc3fdf8ce88694237fad242465b2e95de1a506
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-12-01 10:22:11 +0800

    Update StoreOrderRepository.java

--- com/storeorder/repository/StoreOrderRepository.java
+++ com/storeorder/repository/StoreOrderRepository.java
@@ -1,12 +1,22 @@
 package com.storeorder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.storeorder.entity.StoreOrder;
 
-
 @Repository
 public interface StoreOrderRepository extends JpaRepository<StoreOrder, Integer> {
 
+	@Query("select distinct u.orderId from StoreOrder u")
+	public List<String> findAllUniqueOrderIds();
+
+	@Query("select distinct u.productId  from StoreOrder u")
+	public List<String> findAllUniqueProductIds();
+
+	@Query("select distinct u.customerID from StoreOrder u")
+	public List<String> findAllUniqueCustomerIds();
 }
