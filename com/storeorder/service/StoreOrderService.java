d3e00a54c84dd329458967a1edf10cc2010a0eab
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-12-01 10:22:41 +0800

    Update StoreOrderService.java

--- com/storeorder/service/StoreOrderService.java
+++ com/storeorder/service/StoreOrderService.java
@@ -2,26 +2,27 @@ package com.storeorder.service;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.transaction.Transactional;
-
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import com.storeorder.entity.StoreOrder;
 import com.storeorder.exception.SalesException;
 import com.storeorder.models.StoreOrderDetails;
 import com.storeorder.repository.StoreOrderRepository;
+import com.storeorder.util.PropertyFileConfigurer;
 import com.storeorder.util.csvStoreOrderReader;
 
 @Service
@@ -33,8 +34,33 @@ public class StoreOrderService {
 	@Autowired
 	csvStoreOrderReader csvSalesReader;
 
-	@Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
-	int batchSize;
+	@Autowired
+	PropertyFileConfigurer propertyConfig;
+
+	private List<String> orderIds;
+	private List<String> productIds;
+	private List<String> customerIds;
+	List<StoreOrderDetails> duplicateRecordIdList;
+
+	public StoreOrderService() {
+		orderIds = new ArrayList<>();
+		productIds = new ArrayList<>();
+		customerIds = new ArrayList<>();
+		duplicateRecordIdList = new ArrayList<>();
+	}
+
+	@Transactional(readOnly = true)
+	public void LoadStoreOrderService() {
+
+		// load from table by unique records of Order id, product id and customer id
+		orderIds = storeRepo.findAllUniqueOrderIds();
+		// load from table by unique records of Order id, product id and customer id
+		productIds = storeRepo.findAllUniqueProductIds();
+
+		// load from table by unique records of Order id, product id and customer id
+		customerIds = storeRepo.findAllUniqueCustomerIds();
+
+	}
 
 	@Transactional
 	public StoreOrder saveRecords(StoreOrder storeOrder) {
@@ -50,7 +76,8 @@ public class StoreOrderService {
 		Optional.ofNullable(storeOrder).ifPresent(storeOrd -> storeOrd.forEach(storeOr -> {
 			storeOrderTemp.add(storeOr);
 
-			if (batchCounter.incrementAndGet() % batchSize == 0 || (batchCounter.get() == storeOrd.size())) {
+			if (batchCounter.incrementAndGet() % propertyConfig.getBatchSize() == 0
+					|| (batchCounter.get() == storeOrd.size())) {
 				try {
 					storeRepo.saveAll(storeOrderTemp);
 					storeOrderTemp.clear();
@@ -64,98 +91,26 @@ public class StoreOrderService {
 	}
 
 	public void saveSalesRecords() {
-		ModelMapper modelMapper = new ModelMapper();
-		List<StoreOrderDetails> salesDetails = null;
 
 		try {
-			csvSalesReader.parseCSV();
-			salesDetails = csvSalesReader.getSalesDetailList();
-
-//			salesDetails.stream().filter(Objects::nonNull).forEach(salesDetail -> {
-//				
-//				//filter the duplicate values using the jpa entity
-//				StoreOrder storeOrder = modelMapper.map(salesDetail, StoreOrder.class);
-//
-//				//filter the duplicate values from the table load collections
-//
-//				//any changes in the existing object then update
-//				System.out.println("salesDetail:" + salesDetail);
-//				
-//				System.out.println("storeOrder:" + storeOrder);
-//				
-//				try {
-//				storeRepo.save(storeOrder);
-//				}catch(DataIntegrityViolationException ex) {
-//					System.out.println("exception message:" + ex.getMessage());
-//				}
-//			});
-
-			System.out.println("inside line 1");
-
-			// load from table by unique records of Order id, product id and customer id
-			List<String> orderIds = storeRepo.findAllUniqueOrderIds();
-
-			System.out.println("inside line 2");
-			// load from table by unique records of Order id, product id and customer id
-			List<String> productIds = storeRepo.findAllUniqueProductIds();
-
-			System.out.println("inside line 3");
-			// load from table by unique records of Order id, product id and customer id
-			List<String> customerIds = storeRepo.findAllUniqueCustomerIds();
-
-			// filter the duplicate values using the jpa entity
-			// Type listType = new TypeToken<List<StoreOrder>>() {}.getType();
-			// List<StoreOrder> storeOrderList = modelMapper.map(salesDetails, listType);
-
-			System.out.println("inside line 4");
 			List<StoreOrder> storeOrderList = new ArrayList<>();
-			System.out.println("size:" + salesDetails.size());
 
-			// include the records of unique records
-			Optional.ofNullable(salesDetails).ifPresent(salesDetail -> salesDetail.forEach(salesDetai -> {
-				if (null != salesDetai) {
+			csvSalesReader.parseCSV();
+			List<StoreOrderDetails> salesDetails = csvSalesReader.getSalesDetailList();
 
-					if ((orderIds == null || orderIds.size() == 0) && (productIds == null || productIds.size() == 0)
-							&& (customerIds == null || customerIds.size() == 0)) {
-						StoreOrder storeOrderTemp = modelMapper.map(salesDetai, StoreOrder.class);
-						storeOrderList.add(storeOrderTemp);
-						orderIds.add(storeOrderTemp.getOrderId());
-						productIds.add(storeOrderTemp.getProductId());
-						customerIds.add(storeOrderTemp.getCustomerID());
+			// load existing records from table
+			LoadStoreOrderService();
+			csvSalesReader.parseCSV();
 
-					} else if ((!orderIds.contains(salesDetai.getOrderId()))
-							&& (!productIds.contains(salesDetai.getProductId()))
-							&& (!customerIds.contains(salesDetai.getCustomerId()))) {
-
-						StoreOrder storeOrderTemp = modelMapper.map(salesDetai, StoreOrder.class);
-						storeOrderList.add(storeOrderTemp);
-						orderIds.add(storeOrderTemp.getOrderId());
-						productIds.add(storeOrderTemp.getProductId());
-						customerIds.add(storeOrderTemp.getCustomerID());
-
-					}
-
-				}
-			}));
-
-			System.out.println("size:" + storeOrderList.size());
-			// filter the duplicate values from the table load collect ions
-
-//			Optional.ofNullable(storeOrderList).ifPresent(storeOrderLis -> storeOrderLis.forEach(storeOrderL -> {
-//				if (null != storeOrderL && (null != productIds && productIds.size() > 0)) {
-//
-//					// the records are already available, insert will result in unique constraint
-//					// hence needs to be ignored and removed from csv record list,
-//					// just insert the record with unique values only
-//					if (orderIds.contains(storeOrderL.getOrderId()) || productIds.contains(storeOrderL.getProductId())
-//							|| customerIds.contains(storeOrderL.getCustomerID())) {
-//						storeOrderLis.remove(storeOrderL);
-//					}
-//				}
-//			}));
+			// include and form unique records
+			Optional.ofNullable(salesDetails).ifPresent(
+					salesDetail -> salesDetail.forEach(salesDetai -> formUniqueRecords(storeOrderList, salesDetai)));
 
 			// bulk insert logic
 			saveAllRecords(storeOrderList);
+
+			// record the file
+			fileToLogDuplicateRecords(salesDetails, storeOrderList);
 
 		} catch (FileNotFoundException e) {
 			System.out.println("exception message:" + e.getMessage());
@@ -163,6 +118,68 @@ public class StoreOrderService {
 			System.out.println("exception message:" + e.getMessage());
 		} catch (SalesException s) {
 			System.out.println("exception message:" + s.getMessage());
+		}
+	}
+
+	private boolean isUniqueRecordSet(StoreOrderDetails salesDetail) {
+		if (CollectionUtils.isEmpty(orderIds) && CollectionUtils.isEmpty(productIds)
+				&& CollectionUtils.isEmpty(customerIds)) {
+			return true;
+		} else if ((!orderIds.contains(salesDetail.getOrderId())) && (!productIds.contains(salesDetail.getProductId()))
+				&& (!customerIds.contains(salesDetail.getCustomerId()))) {
+			return true;
+		}
+		return false;
+	}
+
+	private void writeToFile(StoreOrderDetails storeorder, StringBuilder content) {
+
+		content.append("duplicate or null record found for rowid:" + storeorder.getRowId());
+		content.append(" Order id:" + storeorder.getOrderId());
+		content.append(" Product id:" + storeorder.getProductId());
+		content.append(" CustomerId id:" + storeorder.getCustomerId());
+		content.append("\n");
+
+	}
+
+	private void fileToLogDuplicateRecords(List<StoreOrderDetails> salesDetailsList, List<StoreOrder> storeOrderList)
+			throws IOException {
+
+		Path path = Paths.get(propertyConfig.getDuplicateEntryfile());
+		StringBuilder content = new StringBuilder();
+		if (!CollectionUtils.isEmpty(salesDetailsList)) {
+
+			duplicateRecordIdList.stream().filter(Objects::nonNull).forEach(arg0 -> writeToFile(arg0, content));
+		}
+
+		try {
+			Files.write(path, content.toString().getBytes());
+		} catch (IOException e) {
+			throw new SalesException("error while writing duplicate records in a file");
+		}
+	}
+
+	private void formUniqueRecords(List<StoreOrder> storeOrderList, StoreOrderDetails salesDetail) {
+
+		if (null != salesDetail && isUniqueRecordSet(salesDetail)) {
+
+			ModelMapper modelMapper = new ModelMapper();
+			StoreOrder storeOrderTemp = modelMapper.map(salesDetail, StoreOrder.class);
+			storeOrderList.add(storeOrderTemp);
+
+			if (null != storeOrderTemp.getOrderId()) {
+				orderIds.add(storeOrderTemp.getOrderId());
+			}
+
+			if (null != storeOrderTemp.getProductId()) {
+				productIds.add(storeOrderTemp.getProductId());
+			}
+			if (null != storeOrderTemp.getCustomerID()) {
+				customerIds.add(storeOrderTemp.getCustomerID());
+			}
+
+		} else {
+			duplicateRecordIdList.add(salesDetail);
 		}
 	}
 }
